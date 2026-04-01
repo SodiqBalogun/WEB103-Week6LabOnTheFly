5a6909d0a9a0c9995c801f76a7f63179ed110173
SodiqBalogun <sbalogun157@gmail.com>
2026-04-01 18:47:38 -0500

    Just Got to Step 3

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">✈️ On the Fly API</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
